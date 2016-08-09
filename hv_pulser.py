c10bf5efec7fb5899268fdea3f66cd4bfba2c8fd
Kit Matic <culture_shock@kitmatic.com>
2016-08-09 17:53:26 -0500

    with .OC_ACTIVE, CCMR2 register OC output to HIGH, INACTIVE OC was broken

--- hv_pulser.py
+++ hv_pulser.py
@@ -15,38 +15,38 @@ xfmr_pulse_pos_half_cycle = 1
 t1 = pyb.Timer(1, prescaler=11, period=xfmr_pulse_period, mode=Timer.UP)
 
 # xfmr_pulse_half_cycle timing.     (for rollover callback)
-# JP3 output and interrrupt.  Compare generates interrupt:
-t1ch2 = t1.channel(2, pyb.Timer.OC_TOGGLE, compare=xfmr_pulse_period,
-                   polarity=pyb.Timer.HIGH, pin=pyb.Pin.board.JP3)   # working!
+# JP4 output and interrrupt.  Compare generates interrupt:
+t1ch3 = t1.channel(3, pyb.Timer.OC_TOGGLE, compare=xfmr_pulse_period,
+                   polarity=pyb.Timer.HIGH, pin=pyb.Pin.board.JP4)
 
 # Define pins so they can be set with pinx.value()on the fly.
 debug_pin = pyb.Pin('JP6', pyb.Pin.OUT_PP)         # working!
 
 
 # NOT(xfmr_pulse_pos_half_cycle) driver pin.  (negative driving transistor)
-# Compare turns off output pin at xfmr_pulse_w count:
-t1ch1 = t1.channel(1, pyb.Timer.OC_INACTIVE, compare=xfmr_pulse_w,
+# Compare turns on output pin at xfmr_pulse_w count:
+t1ch1 = t1.channel(1, pyb.Timer.OC_ACTIVE, compare=xfmr_pulse_w,
                    polarity=pyb.Timer.HIGH, pin=pyb.Pin.board.JP5)
 
 # xfmr_pulse_pos_half_cycle driver pin.  (positive driving transistor)
-# Compare turns off output pin at xfmr_pulse_w count:
-t1ch3 = t1.channel(3, pyb.Timer.OC_INACTIVE, compare=xfmr_pulse_w,
-                   polarity=pyb.Timer.HIGH, pin=pyb.Pin.board.JP4)
-# For ch3 CCMR3 register OC output to HIGH
-ccmr3 = stm.mem16[stm.TIM1 + stm.TIM_CCMR3]
-ccmr3 &= 0b1111111111011111
-ccmr3 |= 0b0000000001010000
-stm.mem16[stm.TIM1 + stm.TIM_CCMR3] = ccmr3
+# Compare turns on output pin at xfmr_pulse_w count:
+t1ch2 = t1.channel(2, pyb.Timer.OC_ACTIVE, compare=xfmr_pulse_w,
+                   polarity=pyb.Timer.HIGH, pin=pyb.Pin.board.JP3)
+# For t1ch2 CCMR2 reg OC output to HIGH --JP3
+ccmr2 = stm.mem16[stm.TIM1 + stm.TIM_CCMR2]
+ccmr2 &= 0b1111111111011111
+ccmr2 |= 0b0000000001010000
+stm.mem16[stm.TIM1 + stm.TIM_CCMR2] = ccmr2
 
 
-def t1ch2_pulses_start_cb():
+def t1ch3_pulses_start_cb():
     "start pulse rising edge"
     global xfmr_pulse_pos_half_cycle
     if xfmr_pulse_pos_half_cycle == 1:
         # xfmr_pulse positive half_cycle off-time just ended
         # turn on driver for following negative half_cycle
         xfmr_pulse_pos_half_cycle = 0
-        # For ch1 CCMR1 register OC output to HIGH
+        # For t1ch1 CCMR1 reg OC output to HIGH -- JP5
         ccmr1 = stm.mem16[stm.TIM1 + stm.TIM_CCMR1]
         ccmr1 &= 0b1111111111011111
         ccmr1 |= 0b0000000001010000
@@ -55,14 +55,14 @@ def t1ch2_pulses_start_cb():
         # xfmr_pulse negative half_cycle off-time just ended
         # turn on driver for following positive xfmr_pulse_pos_half_cycle = 1
         xfmr_pulse_pos_half_cycle = 1
-        # For ch3 CCMR3 register OC output to HIGH
-        ccmr3 = stm.mem16[stm.TIM1 + stm.TIM_CCMR3]
-        ccmr3 &= 0b1111111111011111
-        ccmr3 |= 0b0000000001010000
-        stm.mem16[stm.TIM1 + stm.TIM_CCMR3] = ccmr3
+        # For t1ch2 CCMR2 reg OC output to HIGH --JP3
+        ccmr2 = stm.mem16[stm.TIM1 + stm.TIM_CCMR2]
+        ccmr2 &= 0b1111111111011111
+        ccmr2 |= 0b0000000001010000
+        stm.mem16[stm.TIM1 + stm.TIM_CCMR2] = ccmr2
 
 
-t1ch2.callback(t1ch2_pulses_start_cb)
+t1ch3.callback(t1ch3_pulses_start_cb)
 
 
 while True:
