1453750bc007452ad4c1afc74389d28faa2a3b22
Kit Matic <culture_shock@kitmatic.com>
2016-08-10 09:18:07 -0500

    hv_pulser script, boot.py

--- boot.py
+++ boot.py
@@ -4,7 +4,6 @@
 import machine
 import pyb
 pyb.freq(96000000)
-pyb.main('hv_pulser.py') # main script to run after this one
-#pyb.usb_mode('CDC+MSC') # act as a serial and a storage device
-#pyb.usb_mode('CDC+HID') # act as a serial device and a mouse
-
+pyb.main('hv_pulser.py')  # main script to run after this one
+# pyb.usb_mode('CDC+MSC')  # act as a serial and a storage device
+# pyb.usb_mode('CDC+HID')  # act as a serial device and a mouse
