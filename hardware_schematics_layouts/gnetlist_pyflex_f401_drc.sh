e6ef0fe76b075e81a73a6c2ad779cb2706090145
Kit Matic <culture_shock@kitmatic.com>
2018-01-24 19:00:50 -0600

    layout progress so all parts placed in pyflex_f401 layout, and schematic lint checked.  kvboard still needs some work.  New USB_B_micro_2leg.lht test_padstack3.lht hdr_mini_1.25_2x5.lht MLF.lht DIOC2013N.lht.

--- hardware_schematics_layouts/gnetlist_pyflex_f401_drc.sh
+++ hardware_schematics_layouts/gnetlist_pyflex_f401_drc.sh
@@ -2,3 +2,4 @@
 # gnetlist_pyflex_f401_drc.sh
 gnetlist -g drc2 pyflex_f401.sch pyflex_f401_boost.sch -o pyflex_f401_all.drc
 gnetlist -g partslist3 pyflex_f401.sch pyflex_f401_boost.sch -o pyflex_f401_all.bom.tsv
+echo " gnetlist_pyflex_f401_drc.sh  -->    pyflex_f401_all.drc"
--- hardware_schematics_layouts/gnetlist_pyflex_f401_net.sh
+++ hardware_schematics_layouts/gnetlist_pyflex_f401_net.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # gnetlist_pyflex_f401_net.sh
 gnetlist -g PCB pyflex_f401.sch pyflex_f401_boost.sch -o pyflex_f401_all.net
+echo " gnetlist_pyflex_f401_net.sh  -->     pyflex_f401_all.net"
