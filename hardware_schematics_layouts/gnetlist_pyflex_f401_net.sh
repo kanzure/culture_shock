d689bed930f91b816f8c6d709532bda847540b66
Kit Matic <culture_shock@kitmatic.com>
2018-02-11 19:07:32 -0600

    Repair BOMs and assembly drawings kvboard.assy+bom.odt pyflex_f401_assy+bom.odt.

--- hardware_schematics_layouts/gnetlist_pyflex_f401_net.sh
+++ hardware_schematics_layouts/gnetlist_pyflex_f401_net.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # gnetlist_pyflex_f401_net.sh
 gnetlist -g PCB pyflex_f401.sch pyflex_f401_boost.sch -o pyflex_f401_all.net
-echo " gnetlist_pyflex_f401_net.sh  -->     pyflex_f401_all.net"
+gnetlist -g partslist3 pyflex_f401.sch pyflex_f401_boost.sch -o pyflex_f401_all.bom.tsv
+echo " gnetlist_pyflex_f401_net.sh  -->     pyflex_f401_all.net --> pyflex_f401_all.bom.tsv  "
