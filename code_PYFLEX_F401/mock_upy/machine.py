f63728d07113bb9ba757126662ce94eca34f152a
Kit Matic <culture_shock@kitmatic.com>
2018-05-09 10:49:31 -0500

    removed mock_upy since from another repo without attribution or license.

--- code_PYFLEX_F401/mock_upy/machine.py
+++
@@ -1,7 +0,0 @@
-class _board(object):
-    def __getattr__(self, *args, **kwargs):
-        return 1
-
-from pyb import Pin as Pin
-from pyb import I2C as I2C
-from stm import mem32, mem16, mem8
--- code_PYFLEX_F401/mock_upy/micropython.py
+++
@@ -1,6 +0,0 @@
-def native(*args, **kwargs):
-    return args[0]
-    pass
-
-def alloc_emergency_exception_buf(*args, **kwargs):
-    pass
