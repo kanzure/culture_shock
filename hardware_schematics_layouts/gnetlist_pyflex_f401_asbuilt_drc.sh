2f7e4968a9a067008e4815fe9291acc0c6d2445b
Kit Matic <culture_shock@kitmatic.com>
2018-08-25 00:35:44 -0500

    Netlist of pyflex_f401_asbuilt_all.net pyflex_f401_asbuilt_all.bom.ods

--- hardware_schematics_layouts/gnetlist_pyflex_f401_asbuilt_drc.sh
+++ hardware_schematics_layouts/gnetlist_pyflex_f401_asbuilt_drc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # gnetlist_pyflex_f401_asbuilt_drc.sh
-lepton-netlist -g drc2 pyflex_f401_asbuilt.sch pyflex_f401_power_asbuilt.sch   -o pyflex_f401_asbuilt.drc
-echo " netlist_pyflex_f401_asbuilt_drc.sh   -->    pyflex_f401_asbuilt.drc"
+gnetlist -g drc2 pyflex_f401_asbuilt.sch pyflex_f401_power_asbuilt.sch   -o pyflex_f401_asbuilt_all.drc
+echo " gnetlist_pyflex_f401_asbuilt_drc.sh   -->    pyflex_f401_asbuilt_all.drc"
